b1cc37eabb5d7f3e37ce029b374b5792b5381454
jiho0127 <111755874+jiho0127@users.noreply.github.com>
2022-08-25 14:26:59 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import "./styles.css";
+
+var rolLength = 6;
+
+const rRandom = () => {
+  var min = Math.ceil(0);
+  var max = Math.floor(rolLength - 1);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const rRotate = () => {
+  var panel = document.querySelector(".rouletter-wacu");
+  var deg = [];
+  for (var i = 1, len = rolLength; i <= len; i++) {
+    deg.push((360 / len) * i);
+  }
+
+  var baseDeg = 7200;
+  var stopDeg = baseDeg + deg[rRandom()];
+  panel.animate(
+    [
+      { transform: "rotate(0deg)" },
+      { transform: "rotate(" + stopDeg + "deg)" }
+    ],
+    {
+      fill: "forwards",
+      duration: 7000,
+      easing: "ease-out"
+    }
+  );
+};
+
+document.addEventListener("click", function (e) {
+  var target = e.target;
+  if (target.tagName === "BUTTON") {
+    rRotate();
+  }
+});
+
+document.getElementById("app").innerHTML = `
+<div class="rouletter">
+    <div class="rouletter-bg">
+        <div class="rouletter-wacu"></div>
+    </div>
+    <div class="rouletter-arrow"></div>
+    <button class="rouletter-btn">start</button>
+</div>
+`;
